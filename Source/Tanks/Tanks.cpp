a3c46612a8e7d00688716014717d31bea01ec1fe
macfisher <macfisher01@gmail.com>
2019-04-20 09:29:29 -0600

    Removed starter content and added a Default level

--- Source/Tanks/Tanks.cpp
+++ Source/Tanks/Tanks.cpp
@@ -4,3 +4,5 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tanks, "Tanks" );
+
+// Assets: https://forums.unrealengine.com/unreal-engine/events/72059-training-stream-tanks-vs-zombies-c-live-from-epic-hq?100199=
